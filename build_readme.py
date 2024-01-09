13217dc61775cfed23f4bd9c54f1c2500b552fb7
DongYubin <892457803@qq.com>
2024-01-09 21:36:37 +0800

    更新

--- build_readme.py
+++ build_readme.py
@@ -1,13 +1,14 @@
-# from python_graphql_client import GraphqlClient
 import feedparser
 import pathlib
 import re
 import os
-import datetime
+from datetime import datetime
+import ssl
+
+if hasattr(ssl, '_create_unverified_context'):
+    ssl._create_default_https_context = ssl._create_unverified_context
 
 root = pathlib.Path(__file__).parent.resolve()
-# client = GraphqlClient(endpoint="https://api.github.com/graphql")
-
 
 TOKEN = os.environ.get("GH_TOKEN", "")
 
@@ -25,93 +26,10 @@ def replace_chunk(content, marker, chunk, inline=False):
 
 def formatGMTime(timestamp):
     GMT_FORMAT = "%a, %d %b %Y %H:%M:%S GMT"
-    dateStr = datetime.datetime.strptime(timestamp, GMT_FORMAT) + datetime.timedelta(
+    dateStr = datetime.strptime(timestamp, GMT_FORMAT) + datetime.timedelta(
         hours=8
     )
     return dateStr.date()
-
-
-# def repository_query(after_cursor=None):
-#     return """
-# query {
-#   viewer {
-#     repositories(first: 100, privacy: PUBLIC, isFork:false, ownerAffiliations:OWNER, after:AFTER) {
-#       pageInfo {
-#         hasNextPage
-#         endCursor
-#       }
-#       nodes {
-#         name
-#         description
-#         url
-#         releases(last: 100, orderBy: { field: CREATED_AT, direction: DESC}) {
-#           totalCount
-#           nodes {
-#             name
-#             publishedAt
-#             url
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
-# """.replace(
-#         "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
-#     )
-
-
-# def fetch_releases(oauth_token):
-#     repos = []
-#     releases = []
-#     repo_names = set()
-#     has_next_page = True
-#     after_cursor = None
-
-#     while has_next_page:
-#         data = client.execute(
-#             query=repository_query(after_cursor),
-#             headers={"Authorization": "Bearer {}".format(oauth_token)},
-#         )
-#         for repo in data["data"]["viewer"]["repositories"]["nodes"]:
-#             if repo["releases"]["totalCount"] and repo["name"] not in repo_names:
-#                 repos.append(repo)
-#                 repo_names.add(repo["name"])
-#                 releases.append(
-#                     {
-#                         "repo": repo["name"],
-#                         "repo_url": repo["url"],
-#                         "description": repo["description"],
-#                         "release": repo["releases"]["nodes"][0]["name"]
-#                         .replace(repo["name"], "")
-#                         .strip(),
-#                         "published_at": repo["releases"]["nodes"][0][
-#                             "publishedAt"
-#                         ].split("T")[0],
-#                         "url": repo["releases"]["nodes"][0]["url"],
-#                     }
-#                 )
-#         has_next_page = data["data"]["viewer"]["repositories"]["pageInfo"][
-#             "hasNextPage"
-#         ]
-#         after_cursor = data["data"]["viewer"]["repositories"]["pageInfo"]["endCursor"]
-#     return releases
-
-# def fetch_weekly():
-#     content = feedparser.parse("https://weekly.tw93.fun/rss.xml")["entries"]
-
-#     entries = [
-#         "* <a href='{url}' target='_blank'>{title}</a> - {published}".format(
-#             title=entry["title"],
-#             url=entry["link"].split("#")[0],
-#             published=datetime.datetime.strptime(
-#                 entry["published"], "%a, %d %b %Y %H:%M:%S %Z"
-#             ).strftime("%Y-%m-%d"),
-#         )
-#         for entry in content
-#     ]
-
-#     return "\n".join(entries[:5])
 
 
 def fetch_blog_entries():
@@ -119,9 +37,7 @@ def fetch_blog_entries():
     return [
         {
             "title": entry["title"],
-            # .split("#")[0]
             "url": entry["link"],
-            # .split("T")[0]
             "published": datetime.strptime(entry["published"], "%a, %d %b %Y %H:%M:%S %z").strftime("%Y-%m-%d %H:%M:%S")
         }
         for entry in entries
@@ -130,46 +46,8 @@ def fetch_blog_entries():
 
 if __name__ == "__main__":
     readme = root / "README.md"
-    # project_releases = root / "releases.md"
-    # releases = fetch_releases(TOKEN)
-    # releases.sort(key=lambda r: r["published_at"], reverse=True)
-    # md = "\n".join(
-    #     [
-    #         "* <a href='{url}' target='_blank'>{repo} {release}</a> - {published_at}".format(
-    #             **release
-    #         )
-    #         for release in releases[:5]
-    #     ]
-    # )
-    # md = "\n"
     readme_contents = readme.open().read()
-    # rewritten = replace_chunk(readme_contents, "recent_releases", md)
-
-    # # Write out full project-releases.md file
-    # project_releases_md = "\n".join(
-    #     [
-    #         (
-    #             "* **[{repo}]({repo_url})**: [{release}]({url}) - {published_at}\n"
-    #             "<br>{description}"
-    #         ).format(**release)
-    #         for release in releases
-    #     ]
-    # )
-    # project_releases_content = project_releases.open().read()
-    # project_releases_content = replace_chunk(
-    #     project_releases_content, "recent_releases", project_releases_md
-    # )
-    # project_releases_content = replace_chunk(
-    #     project_releases_content, "release_count", str(len(releases)), inline=True
-    # )
-    # project_releases.open("w").write(project_releases_content)
-
-    # print("fetch_weekly>>>>>>",fetch_weekly())
-
-    # weekly_text = "\n" + fetch_weekly()
-    # rewritten = replace_chunk(rewritten, "weekly", weekly_text)
-
-    entries = fetch_blog_entries()[:5]
+    entries = fetch_blog_entries()
     entries_md = "\n".join(
         [
             "* <a href='{url}' target='_blank'>{title}</a> - {published}".format(
@@ -179,5 +57,5 @@ if __name__ == "__main__":
         ]
     )
     rewritten = replace_chunk(readme_contents, "blog", entries_md)
-
+    # print(rewritten)
     readme.open("w").write(rewritten)
